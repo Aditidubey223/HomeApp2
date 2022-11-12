67e4aaeb10df696b1e70d9aafb69a7bb41c41c9e
aditi dubey <aditidubey9669@gmail.com>
2022-11-12 13:39:58 +0530

    xyz

--- homeapp/app/src/main/java/com/example/homeapp/login.java
+++ homeapp/app/src/main/java/com/example/homeapp/login.java
@@ -1,13 +1,17 @@
 package com.example.homeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class dashboard extends AppCompatActivity {
+public class login extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard);
+        setContentView(R.layout.login);
+
+
     }
 }
